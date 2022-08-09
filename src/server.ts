5552844921eabc9fc4efbb7acfad246634078deb
Matt Lobo Cardoso <matheuscardoso1@poli.ufrj.br>
2022-08-09 00:15:15 -0300

    add swagger docs

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
 import { DistancesController } from "@controllers/DistancesController";
 
 const app = express();
 
+app.use(express.json());
 app.get("/distances", DistancesController.get);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
